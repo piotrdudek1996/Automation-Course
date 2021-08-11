1dc1805ea63e37ee10918de8c511758dec060156
piotrdudek1996 <piotr.dudek1996@gmail.com>
2021-08-11 17:04:46 +0200

    Lesson 1 files to pull request

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('Hello!');
+
+var str = 'Jestem stringiem.';
+let str2 = ' Też jestem stringiem.';
+const str3 = ' Ale super, ja też.';
+console.log(str+str2+str3);
+let number1 = 4;
+let number2 = 7;
+let bigInt3 = 56456456546464^5;
+let number4 = 324424242;
+let number5 = 10;
+let number6 = 6764;
+let number7 = 3;
+console.log(number1+number2);
+let result = number2 * number1;
+console.log(result);
+console.log(typeof result);
+console.log(bigInt3/number6); // wywala tu dziwny wynik na minusie -> dlaczego? za duże liczby?
+console.log(number1+number2*number5-number7); 
+
+
+
+// tu sobie wpiszę komentarz w jednej linijce
+/* tu też komentarz w jednej linijce */
+/*szaleństwo 
+tu
+komentarz 
+w kilku
+linijkach 
+*/
+
